410b76fe29014dca80c8b552576ad49689ca3fe1
DGTV11 <danielweedk@gmail.com>
2024-03-17 11:26:08 +0800

    Updated Modelfile and main.py

--- main.py
+++ main.py
@@ -2,12 +2,6 @@ import gradio as gr
 import ollama, os
 from pptx import Presentation
 
-print('Checking if flashcards_helper model exists...')
-if 'flashcards_helper:latest' not in [model['name'] for model in ollama.list()['models']]:
-	with open(os.path.join(os.path.dirname(__file__), 'Modelfile')) as file:
-		print('Creating flashcards_helper from modelfile string...')
-		ollama.create(model='flashcards_helper', modelfile=file.read())
-
 def gen_flashcards(filepaths, notes):
 	pptx_files_notes = ''
 	if filepaths:
@@ -34,26 +28,40 @@ def gen_flashcards(filepaths, notes):
 		res_stream += chunk['message']['content']
 		yield res_stream
 
-with gr.Blocks(theme=gr.themes.Default(primary_hue="red")) as studyCopilot:
-	gr.Markdown(
-	"""
-	# Study Copilot
-	""")
+def regen_flashcards_helper():
+	with open(os.path.join(os.path.dirname(__file__), 'Modelfile')) as file:
+		gr.Info('Regenerating flashcards_helper from Modelfile...')
+		ollama.create(model='flashcards_helper', modelfile=file.read())
 
-	with gr.Tab('Flashcard Generator'):
-		inp_cards_slides = gr.File(label="Slides:", file_count='multiple', file_types=['.pptx'])	
-		inp_cards_words = gr.Textbox(label="Notes:")
+if __name__ == '__main__':
+	print('Checking if flashcards_helper model exists...')
+	if 'flashcards_helper:latest' not in [model['name'] for model in ollama.list()['models']]:
+		with open(os.path.join(os.path.dirname(__file__), 'Modelfile')) as file:
+			print('Creating flashcards_helper from Modelfile...')
+			ollama.create(model='flashcards_helper', modelfile=file.read())	
 
-		with gr.Row():
-			clear_inputs_btn = gr.ClearButton(components=[inp_cards_slides, inp_cards_words])
-			generate_cards_btn = gr.Button(value="Generate Flashcards")
-		
-		stop = gr.Button(value="Stop", variant='primary')
-		out_cards = gr.Markdown(label="A deck of flashcards:")
-		clear_cards_btn = gr.ClearButton(components=[out_cards])
+	with gr.Blocks(theme=gr.themes.Default(primary_hue="red")) as studyCopilot:
+		gr.Markdown(
+		"""
+		# Study Copilot
+		""")
 
-	gen_cards_event = generate_cards_btn.click(fn=gen_flashcards, inputs=[inp_cards_slides, inp_cards_words], outputs=[out_cards])
-	stop.click(fn=None, inputs=None, outputs=None, cancels=[gen_cards_event])
+		with gr.Tab('Flashcard Generator'):
+			inp_cards_slides = gr.File(label="Slides:", file_count='multiple', file_types=['.pptx'])	
+			inp_cards_words = gr.Textbox(label="Notes:")
 
-print('Launching...')
-studyCopilot.launch()
+			with gr.Row():
+				clear_inputs_btn = gr.ClearButton(components=[inp_cards_slides, inp_cards_words])
+				generate_cards_btn = gr.Button(value="Generate Flashcards")
+			
+			stop = gr.Button(value="Stop", variant='primary')
+			out_cards = gr.Markdown(label="A deck of flashcards:")
+			clear_cards_btn = gr.ClearButton(components=[out_cards])
+			regen_flashcards_helper_btn = gr.Button(value="Regenerate flashcards_helper", variant='secondary')
+
+		regen_flashcards_helper_btn.click(fn=regen_flashcards_helper, inputs=[], outputs=[])
+		gen_cards_event = generate_cards_btn.click(fn=gen_flashcards, inputs=[inp_cards_slides, inp_cards_words], outputs=[out_cards])
+		stop.click(fn=None, inputs=None, outputs=None, cancels=[gen_cards_event])
+
+	print('Launching...')
+	studyCopilot.launch()
