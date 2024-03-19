7809e1104f89e02bad83699076b4309b72931844
DGTV11 <danielweedk@gmail.com>
2024-03-19 18:33:17 +0800

    Updated moderate.py

--- modules/moderate.py
+++ modules/moderate.py
@@ -1,15 +1,16 @@
 import ollama
 import gradio as gr
 
-def moderate(prompt, raise_error_if_moderated=True) -> None:
-    response = ollama.chat(
+def moderate(prompt: str) -> str:
+    return ollama.chat(
         model='moderator',
         messages=[{'role': 'user', 'content': f'Text: {prompt}'}],
         stream=False,
-    )
+    )['message']['content']
 
-    if 'not moderated' in response.lower():
+def check_moderation(response_str: str, raise_error_if_moderated: bool=True) -> None:
+    if 'not moderated' in response_str.lower():
         return None
     if raise_error_if_moderated:
-        raise gr.Error(f'Content moderated! Reason: \'{response}\'')
-    gr.Warning(f'Content moderated! Read at your own risk! Reason: \'{response}\'')
+        raise gr.Error(f'Content moderated! Reason: \'{response_str}\'')
+    gr.Warning(f'Content moderated! Read at your own risk! Reason: \'{response_str}\'')
