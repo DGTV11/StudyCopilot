8310e6522efb607e41731ec93f98f898d3e5f463
DGTV11 <danielweedk@gmail.com>
2024-03-27 12:45:58 +0800

    Made minor changes in server/server.py

--- server/server.py
+++ server/server.py
@@ -2,13 +2,12 @@ from os import path
 
 from pyngrok import conf, ngrok
 
-# ngrok http 11434 --host-header="localhost:11434" --domain=pleasing-precisely-sawfly.ngrok-free.app
+conf.get_default().monitor_thread = False
 
 with open(path.join(path.dirname(path.dirname(__file__)), 'server-url.txt'), 'r') as f:
     http_tunnel = ngrok.connect(addr=11434, proto='http', host_header="localhost:11434", domain=f.read())
 
-conf.get_default().monitor_thread = False
-
+# ngrok http 11434 --host-header="localhost:11434" --domain=pleasing-precisely-sawfly.ngrok-free.app
 ngrok_process = ngrok.get_ngrok_process()
 
 print(f'HTTP tunnel: {http_tunnel.public_url}')
@@ -18,5 +17,4 @@ try:
     ngrok_process.proc.wait()
 except KeyboardInterrupt:
     print("Shutting down server.")
-
     ngrok.kill()
